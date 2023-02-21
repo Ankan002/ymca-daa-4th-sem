222304f7141d0cad8962ffeb813ce881c8422e93
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-02-21 11:21:59 +0530

    feat: :sparkles: add color encoding

--- sorting/assignment-1/index.ts
+++ sorting/assignment-1/index.ts
@@ -1,4 +1,5 @@
 import { bubbleSort } from "./bubble-sort";
+import clc from "cli-color";
 
 const arrayOne: Array<number> = [];
 
@@ -6,12 +7,15 @@ for(let i=0; i<100000; i++){
     arrayOne.push(Math.floor(Math.random() * 10000) + 10);
 }
 
+console.log(clc.yellowBright("Actual Array:"))
+console.log(arrayOne)
+
 const startTimeOne = Date.now();
 bubbleSort(arrayOne);
 const endTimeOne = Date.now();
 
-console.log("Bubble Sort:")
-console.log(`Time required: ${(endTimeOne - startTimeOne) / 1000} s`);
+console.log(clc.redBright("Bubble Sort:"))
+console.log(clc.greenBright(`Time required: ${(endTimeOne - startTimeOne) / 1000} s`));
 console.log(arrayOne);
 
 const arrayTwo = JSON.parse(JSON.stringify(arrayOne));
